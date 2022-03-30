c0bdc7fafcec04426dc3e4171b4f37d04ec5b821
Adrian Karwowski <adziok1998@gmail.com>
2022-03-30 21:55:14 +0200

    chore: add style config to shared lib

---
+++ packages/shared/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+  },
+  extends: [
+    '../../.eslintrc.js'
+  ],
+  ignorePatterns: ['lib'],
+};
