8b3026674ff3ab0a0349c8911d6925bde3cdc475
Adrian Karwowski <adziok1998@gmail.com>
2022-03-30 21:52:44 +0200

    chore: add tailwind chore: create shared library chore: standardize packages names

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <div className="p-6 max-w-sm mx-auto bg-green-400 rounded-xl shadow-lg flex items-center space-x-4 text-center">
+            <header>
+                <img src={logo} alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
             </header>
