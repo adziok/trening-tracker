ad22c2a04b3691f18879f5a25ce8ac69c4581b79
Adrian Karwowski <adziok1998@gmail.com>
2022-04-01 16:42:31 +0200

    feat: add utils to DateUtils

--- packages/backend/src/shared/classes/DateUtils.ts
+++ packages/backend/src/shared/classes/DateUtils.ts
@@ -1,5 +1,36 @@
+type TimeType = 'milliseconds' | 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks';
+
+const timeTypeToMilliseconds: Record<TimeType, number> = {
+    milliseconds: 1,
+    seconds: 1000,
+    minutes: 60 * 1000,
+    hours: 60 * 60 * 1000,
+    days: 24 * 60 * 60 * 1000,
+    weeks: 7 * 24 * 60 * 60 * 1000,
+};
+
 export class DateUtils {
     static now(): Date {
         return new Date();
+    }
+
+    static fromTimestamp(timestamp: number): Date {
+        return new Date(timestamp);
+    }
+
+    static before(baseDate: Date, checkedDate: Date) {
+        return baseDate.getTime() < checkedDate.getTime();
+    }
+
+    static after(baseDate: Date, checkedDate: Date) {
+        return baseDate.getTime() > checkedDate.getTime();
+    }
+
+    static addTimeToDate(initData: Date, value: number, type: TimeType): Date {
+        return DateUtils.fromTimestamp(initData.getTime() + timeTypeToMilliseconds[type] * value);
+    }
+
+    static getMillisecondOfType(type: TimeType): number {
+        return timeTypeToMilliseconds[type];
     }
 }
