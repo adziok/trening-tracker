9f5bc80b2155e8efbad9ae36cf0a56597764cbde
Adrian Karwowski <adziok1998@gmail.com>
2022-04-09 17:56:37 +0200

    feat(presentation): register TrainingController

--- packages/backend/src/presentation/PresentationModule.ts
+++ packages/backend/src/presentation/PresentationModule.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './AccountsController';
+import { TrainingController } from './training/TrainingController';
+import { TrainingModule } from '../modules/training/TrainingModule';
 
 @Module({
-    controllers: [AccountsController],
+    imports: [TrainingModule],
+    controllers: [AccountsController, TrainingController],
 })
 export class PresentationModule {}
