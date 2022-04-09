bd20d8b7fc245f3ce17e53e8609373f2673cbd20
Adrian Karwowski <adziok1998@gmail.com>
2022-04-09 17:02:04 +0200

    feat(auth): save user in request when token is valid

--- packages/backend/src/modules/auth/cognito/CognitoAccessTokenGuard.ts
+++ packages/backend/src/modules/auth/cognito/CognitoAccessTokenGuard.ts
@@ -1,21 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtVerifier } from '../JwtVerifier';
+import { IAccountsApi } from '../../accounts/application/AccountsApi';
 
 @Injectable()
 export class CognitoAccessTokenGuard implements CanActivate {
-    constructor(private readonly verifier: JwtVerifier) {}
-
-    static baseUrl(region: string, userPoolId: string): string {
-        return `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
-    }
+    constructor(private readonly accountFacade: IAccountsApi, private readonly verifier: JwtVerifier) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         try {
             const request = context.switchToHttp().getRequest<Request>();
             const token = request.header('Authorization')?.replace('Bearer ', '');
 
-            await this.verifier.verifyOrThrow(token);
+            const value = await this.verifier.verifyOrThrow(token);
+            request.user = this.accountFacade.getAccountByProvider('cognito', value.sub);
             return true;
         } catch (e) {
             console.log(e);
