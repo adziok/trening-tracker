b0cd02d3aee38fc1e2b5b8027c32c7e4f681da89
Adrian Karwowski <adziok1998@gmail.com>
2022-04-09 16:54:03 +0200

    feat(account): make AccountsModule.ts global to allow use account facade in authorization flow

--- packages/backend/src/modules/accounts/AccountsModule.ts
+++ packages/backend/src/modules/accounts/AccountsModule.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { IAccountsApi } from './application/AccountsApi';
 import { AccountService } from './application/AccountService';
 import { AccountRepository } from './application/AccountRepository';
 import { AccountInMemoryRepository } from './infra/AccountInMemoryRepository';
 
+@Global()
 @Module({
     imports: [],
     controllers: [],
