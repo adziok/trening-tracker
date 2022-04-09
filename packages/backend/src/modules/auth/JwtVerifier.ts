479ea2ae924f12d6641790d1f0cc43a11a5cc97b
Adrian Karwowski <adziok1998@gmail.com>
2022-04-09 12:08:24 +0200

    refactor: add typing to JwtVerifier

--- packages/backend/src/modules/auth/JwtVerifier.ts
+++ packages/backend/src/modules/auth/JwtVerifier.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CognitoJwtVerifier } from 'aws-jwt-verify';
 import { AuthConfigService } from './AuthConfigService';
+import { CognitoAccessTokenPayload } from 'aws-jwt-verify/jwt-model';
+
+export type VerifiedPayload = {
+    sub: string;
+    iss: string;
+    version: number;
+    client_id: string;
+    origin_jti: string;
+    token_use: 'access';
+    scope: string;
+    auth_time: number;
+    exp: number;
+    iat: number;
+    jti: string;
+    username: string;
+};
 
 @Injectable()
 export class JwtVerifier {
-    private verifier: { verify: (token: string) => any };
+    private verifier: { verify: (token: string) => Promise<CognitoAccessTokenPayload> };
 
     constructor(private readonly config: AuthConfigService) {
         this.verifier = CognitoJwtVerifier.create({
@@ -14,7 +30,7 @@ export class JwtVerifier {
         });
     }
 
-    async verify(token: string): Promise<void> {
-        await this.verifier.verify(token);
+    verify(token: string): Promise<VerifiedPayload> {
+        return this.verifier.verify(token);
     }
 }
