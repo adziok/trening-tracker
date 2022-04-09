019cf09774c23023b0630875665a0c667ecf109e
Adrian Karwowski <adziok1998@gmail.com>
2022-04-09 17:25:51 +0200

    refactor(presentation): use valid account id in createTraining method

--- packages/backend/src/presentation/training/TrainingController.ts
+++ packages/backend/src/presentation/training/TrainingController.ts
@@ -3,6 +3,8 @@ import { TrainingFacade } from '../../modules/training/TrainingFacade';
 import { Authorized } from '../../modules/auth';
 import { MutableActionResultDto } from '../common/MutableActionResultDto';
 import { CreateTrainingDto } from './dtos/CreateTrainingDto';
+import { CurrentAccount } from '../../modules/auth/CurrentAccount';
+import { TAccount } from '../../modules/accounts/application/AccountFacade';
 
 @Authorized()
 @Controller('training')
@@ -10,7 +12,10 @@ export class TrainingController {
     constructor(private readonly trainingFacade: TrainingFacade) {}
 
     @Post()
-    async createTraining(@Body() data: CreateTrainingDto): Promise<MutableActionResultDto> {
-        return { id: await this.trainingFacade.createTraining({ ...data, accountId: '' }) };
+    async createTraining(
+        @CurrentAccount() account: TAccount,
+        @Body() data: CreateTrainingDto
+    ): Promise<MutableActionResultDto> {
+        return { id: await this.trainingFacade.createTraining({ ...data, accountId: account.id }) };
     }
 }
