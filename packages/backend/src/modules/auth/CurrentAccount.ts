8eaf4b8ccc2beb42f23b0a5455b04f42008b3780
Adrian Karwowski <adziok1998@gmail.com>
2022-04-09 17:08:37 +0200

    fix(account): fix CurrentAccount.ts

--- packages/backend/src/modules/auth/CurrentAccount.ts
+++ packages/backend/src/modules/auth/CurrentAccount.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { TAccount } from '../accounts/application/AccountFacade';
 import { Request } from 'express';
 
-export const CurrentAccount = createParamDecorator((ctx: ExecutionContext): TAccount => {
+export const CurrentAccount = createParamDecorator((data: unknown, ctx: ExecutionContext): TAccount => {
     const req = ctx.switchToHttp().getRequest<Request>();
     return req.user as TAccount;
 });
