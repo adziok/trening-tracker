f59cd7212dadd8ddc32eba1facc3172baeaa75eb
Adrian Karwowski <adziok1998@gmail.com>
2022-04-09 13:09:45 +0200

    list: ignore eslint errors test: fix mockClassWithDefaultThrow

--- packages/backend/test/common/mockClassWithDefaultThrow.ts
+++ packages/backend/test/common/mockClassWithDefaultThrow.ts
@@ -1,10 +1,5 @@
 type ClassRef = Record<string, any>;
 
-type Type<T> = Record<string, unknown> & {
-    new (...args: any[]): T;
-    prototype: Record<string, unknown>;
-};
-
 type FunctionPropertyNames<T> = {
     // eslint-disable-next-line @typescript-eslint/ban-types
     [K in keyof T]: T[K] extends Function ? K : never;
@@ -13,13 +8,15 @@ type FunctionPropertyNames<T> = {
 type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
 
 export const mockClassWithDefaultThrow = <T extends ClassRef>(
-    classToMock: Type<T>,
+    classToMock: any,
     overrideMethods: Partial<FunctionProperties<T>> = {}
 ): jest.Mocked<T> => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
     return Reflect.ownKeys(classToMock.prototype)
         .filter((key) => key !== 'constructor')
         .map((key) => ({
             [key]: jest.fn().mockImplementation(() => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
                 throw new Error(`Method ${classToMock.name}->${key.toString()}(): is not implemented`);
             }),
         }))
