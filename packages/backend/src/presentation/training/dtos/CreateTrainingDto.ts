9285980362c25af8b0cbefe6dc84dd05f4505b56
Adrian Karwowski <adziok1998@gmail.com>
2022-04-10 11:37:20 +0200

    feat(presentation): use shared interface as dto base

--- packages/backend/src/presentation/training/dtos/CreateTrainingDto.ts
+++ packages/backend/src/presentation/training/dtos/CreateTrainingDto.ts
@@ -1,6 +1,7 @@
 import { IsDate, IsOptional, IsString, Length } from 'class-validator';
+import { ICreateTrainingDto } from '@trening-tracker/shared';
 
-export class CreateTrainingDto {
+export class CreateTrainingDto implements ICreateTrainingDto {
     @IsString()
     @Length(3, 100)
     name: string;
