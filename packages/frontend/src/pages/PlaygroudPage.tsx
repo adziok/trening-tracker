6ecabb2d32032e1ca2ff65181f18bbb2cfc39ebd
Adrian Karwowski <adziok1998@gmail.com>
2022-04-10 12:07:53 +0200

    feat(PlaygroundPage): user createTrainingMutation, create simple view

--- packages/frontend/src/pages/PlaygroudPage.tsx
+++ packages/frontend/src/pages/PlaygroudPage.tsx
@@ -1,39 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Drawer, Group, Text, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { DatePicker } from '@mantine/dates';
+import { ICreateTrainingDto } from '@trening-tracker/shared';
 import { PageWrapper } from '../components';
-import { Button } from '@mantine/core';
+import { createTrainingMutation } from '../hooks';
 
 export function PlaygroundPage() {
-    return (
-        <PageWrapper className="bg-theme-3 bg-bg">
-            <div className="bg-mountains h-screen w-screen bg-repeat-x fixed bg-bottom z-10 bg-a-20p bg-[length:200%] sm:bg-[length:100%]">
-                {' '}
-            </div>
-            <div className="bg-clouds h-screen animate-moveRight w-screen bg-repeat-x fixed bg-bottom z-10 bg-a-30p bg-50p opacity-30">
-                {' '}
-            </div>
+    const [opened, setOpened] = useState(false);
+    const form = useForm<ICreateTrainingDto>({
+        initialValues: {
+            name: '',
+            startedAt: undefined,
+        },
+    });
+    const { mutate, isLoading, isSuccess, isError } = createTrainingMutation();
 
-            <div className="fixed h-1/2 z-50 w-screen -bottom-0">
-                <div className="flex flex-col p-5 h-full">
-                    <div className="flex flex-row">Name:</div>
-                    <div className="flex flex-row">
-                        <input className="w-full" type="text" />
-                    </div>
-                    <div className="flex flex-row">Time:</div>
-                    <div className="flex flex-row">
-                        <input className="w-full" type="datetime-local" />
-                        <Button size="sm" variant={'light'}>
-                            Now
-                        </Button>
-                    </div>
-                    <div className="flex-grow"></div>
-                    <div className="flex flex-row justify-between">
-                        <Button size="sm" variant={'filled'}>
-                            Cancel
-                        </Button>
-                        <Button size="sm">Save</Button>
-                    </div>
-                </div>
-            </div>
+    useEffect(() => {
+        if (!isLoading && isSuccess) {
+            console.log('Success');
+            form.reset();
+            setOpened(false);
+        }
+        if (!isLoading && isError) {
+            console.log('Error');
+        }
+    }, [isLoading]);
+
+    const createTraining = (data: ICreateTrainingDto) => {
+        mutate(data);
+    };
+
+    return (
+        <PageWrapper>
+            <Button onClick={() => setOpened(true)}>Create training</Button>
+            <Drawer position="bottom" opened={opened} withCloseButton={false} onClose={() => setOpened(false)}>
+                <Box sx={{ maxWidth: 300 }} mx="auto">
+                    <form onSubmit={form.onSubmit((values) => createTraining(values))}>
+                        <Text size={'xl'} className="pt-2">
+                            Create new training:
+                        </Text>
+                        <TextInput required label="Title" placeholder="Training name" {...form.getInputProps('name')} />
+                        <DatePicker
+                            required
+                            label="Training date"
+                            placeholder="Training date"
+                            defaultValue={new Date()}
+                            {...form.getInputProps('startedAt')}
+                        />
+
+                        <Group position="apart" mt="md">
+                            <Button
+                                color={'red'}
+                                variant={'light'}
+                                onClick={() => setOpened(false)}
+                                loading={isLoading}
+                            >
+                                Cancel
+                            </Button>
+                            <Button type="submit" variant={'light'} loading={isLoading}>
+                                Submit
+                            </Button>
+                        </Group>
+                    </form>
+                </Box>
+            </Drawer>
         </PageWrapper>
     );
 }
