9828e749be7f6907e549ce17aecd239436c439bf
Adrian Karwowski <adziok1998@gmail.com>
2022-04-10 11:39:17 +0200

    fix(useMe): add types

--- packages/frontend/src/hooks/UseMe.ts
+++ packages/frontend/src/hooks/UseMe.ts
@@ -2,5 +2,7 @@ import { useQuery } from 'react-query';
 import { fetchBackendWithAuthorization } from '../utils/FetchBackendWithAuthorization';
 
 export const useMe = () => {
-    return useQuery<{ id: string }, { error: string }>('me', () => fetchBackendWithAuthorization.get('accounts/me'));
+    return useQuery<{ id: string }, { error: string }>('me', () =>
+        fetchBackendWithAuthorization.get<{ id: string }>('accounts/me')
+    );
 };
