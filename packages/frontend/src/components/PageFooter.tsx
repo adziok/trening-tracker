f1ddefd9b0e3c25fad5fc5767b6033906e7563a2
Adrian Karwowski <adziok1998@gmail.com>
2022-04-10 16:09:50 +0200

    feat(components): create page footer

---
+++ packages/frontend/src/components/PageFooter.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+import { Container, Group } from '@mantine/core';
+
+type PageHeaderProps = {
+    children: ReactNode;
+    position?: 'apart' | 'center' | 'left' | 'right';
+};
+
+export const PageFooter = ({ children, position = 'center' }: PageHeaderProps) => {
+    return (
+        <Container className={'border-red-500 border-0 border-t-2 border-solid box-border w-full py-2'}>
+            <Group position={position}>{children}</Group>
+        </Container>
+    );
+};
--- packages/frontend/src/components/PageWrapper.tsx
+++ packages/frontend/src/components/PageWrapper.tsx
@@ -2,17 +2,18 @@ import React, { ReactNode } from 'react';
 import { Container, Stack } from '@mantine/core';
 import { PageHeader } from './PageHeader';
 
-type PageWrapperProps = { header?: ReactNode; children: ReactNode; className?: string };
+type PageWrapperProps = { header?: ReactNode; children: ReactNode; className?: string; footer?: ReactNode };
 
 const ScrollableContent = ({ children }: PageWrapperProps) => {
-    return <Container className={`h-full box-border w-full overflow-x-hidden`}>{children}</Container>;
+    return <Container className={`h-full box-border w-full overflow-x-hidden overflow-y-auto`}>{children}</Container>;
 };
 
-export const PageWrapper = ({ children, className = ' ', header = <PageHeader /> }: PageWrapperProps) => {
+export const PageWrapper = ({ children, className = '', header = <PageHeader />, footer }: PageWrapperProps) => {
     return (
         <Stack className={'h-screen overflow-hidden'} align={'stretch'} spacing={'xs'}>
             {header}
             <ScrollableContent>{children}</ScrollableContent>
+            {footer && footer}
         </Stack>
     );
 };
