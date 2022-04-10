c6fe4662bcff0ac546dc4be6ea697acdd34bc2bb
Adrian Karwowski <adziok1998@gmail.com>
2022-04-10 11:38:26 +0200

    feat(mantine): change base font family, remove wrong import

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { MantineProvider } from '@mantine/core';
 import './index.css';
-import './tailwind.output.css';
 import reportWebVitals from './reportWebVitals';
 import { AppRoutes } from './routes/AppRoutes';
 
@@ -16,7 +15,8 @@ ReactDOM.render(
         <MantineProvider
             theme={{
                 // Override any other properties from default theme
-                fontFamily: 'Open Sans, sans serif',
+                fontFamily:
+                    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
                 spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
             }}
         >
