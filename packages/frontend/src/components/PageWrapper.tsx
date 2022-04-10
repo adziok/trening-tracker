42cd396c7991db51ab67df0d4bf0c223181e648b
Adrian Karwowski <adziok1998@gmail.com>
2022-04-10 14:49:46 +0200

    feat(components): refactor PageWrapper.tsx

--- packages/frontend/src/components/PageWrapper.tsx
+++ packages/frontend/src/components/PageWrapper.tsx
@@ -1,11 +1,18 @@
 import React, { ReactNode } from 'react';
-import { Container } from '@mantine/core';
+import { Container, Stack } from '@mantine/core';
+import { PageHeader } from './PageHeader';
 
-type PageWrapperProps = {
-    children: ReactNode;
-    className?: string;
+type PageWrapperProps = { header?: ReactNode; children: ReactNode; className?: string };
+
+const ScrollableContent = ({ children }: PageWrapperProps) => {
+    return <Container className={`h-full box-border w-full overflow-x-hidden`}>{children}</Container>;
 };
 
-export const PageWrapper = ({ children, className = '' }: PageWrapperProps) => {
-    return <Container className={`${className}`}>{children}</Container>;
+export const PageWrapper = ({ children, className = ' ', header = <PageHeader /> }: PageWrapperProps) => {
+    return (
+        <Stack className={'h-screen overflow-hidden'} align={'stretch'} spacing={'xs'}>
+            {header}
+            <ScrollableContent>{children}</ScrollableContent>
+        </Stack>
+    );
 };
