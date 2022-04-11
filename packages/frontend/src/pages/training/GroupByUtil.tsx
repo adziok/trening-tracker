dcef14db1cac6f79becff9b875a2719f9b7de477
Adrian Karwowski <adziok1998@gmail.com>
2022-04-11 14:44:44 +0200

    feat: groupBy date

--- packages/frontend/src/pages/training/GroupByUtil.tsx
+++ packages/frontend/src/pages/training/GroupByUtil.tsx
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 export const groupBy = <ArrayElement extends Record<string, unknown>, GroupKey extends keyof ArrayElement>(
     array: ArrayElement[],
     key: GroupKey
@@ -5,14 +7,17 @@ export const groupBy = <ArrayElement extends Record<string, unknown>, GroupKey e
     array.reduce((previousValue, currentValue) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        if (previousValue?.[currentValue[key]]) {
+        const groupByKeyValue = dayjs(currentValue[key]).format('YYYY-MM-DD');
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (previousValue?.[groupByKeyValue]) {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-            previousValue?.[currentValue[key]].push(currentValue);
+            previousValue?.[groupByKeyValue].push(currentValue);
             return previousValue;
         }
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        return { ...previousValue, [currentValue[key]]: [currentValue] };
+        return { ...previousValue, [groupByKeyValue]: [currentValue] };
     }, {} as Record<GroupKey, ArrayElement[]>);
