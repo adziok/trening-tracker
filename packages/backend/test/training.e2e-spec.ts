b4daf1b9ddd6285e59dad299c4dd6eb22fc6237c
Adrian Karwowski <adziok1998@gmail.com>
2022-04-11 21:41:49 +0200

    test: list trainings, get training

--- packages/backend/test/training.e2e-spec.ts
+++ packages/backend/test/training.e2e-spec.ts
@@ -2,6 +2,23 @@ import { createTestingApp } from './common';
 import { UserSession } from './common/UserSessionFactory';
 import { Response } from 'supertest';
 
+const data = {};
+jest.mock('../src/shared/FSWrapper.ts', () => {
+    return {
+        FSWrapper: {
+            writeFileSync(path: string, _data: string) {
+                data[path] = _data;
+            },
+            readFileSync(path: string): string {
+                return data[path] as string;
+            },
+            existsSync(path: string) {
+                return !!data[path];
+            },
+        },
+    };
+});
+
 describe('AppController (e2e)', () => {
     let session: UserSession;
 
@@ -32,6 +49,58 @@ describe('AppController (e2e)', () => {
             })
             .expect((res: Response) => {
                 expect(res.body.id).toBeDefined();
+            });
+    });
+
+    it('should list trainings', async () => {
+        await session.post('/training').set(session.authorizationHeaders()).send({
+            name: 'First training',
+            startedAt: new Date(),
+        });
+        await session.post('/training').set(session.authorizationHeaders()).send({
+            name: 'Second training',
+            startedAt: new Date(),
+        });
+
+        await session
+            .get('/training')
+            .set(session.authorizationHeaders())
+            .send()
+            .expect((res) => {
+                expect(res.body.nodes).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            id: expect.any(String),
+                            name: 'First training',
+                        }),
+                        expect.objectContaining({
+                            id: expect.any(String),
+                            name: 'Second training',
+                        }),
+                    ])
+                );
+            });
+    });
+
+    it('should get training by id', async () => {
+        const {
+            body: { id },
+        }: { body: { id: string } } = await session.post('/training').set(session.authorizationHeaders()).send({
+            name: 'First training',
+            startedAt: new Date(),
+        });
+
+        await session
+            .get(`/training/${id}`)
+            .set(session.authorizationHeaders())
+            .send()
+            .expect((res) => {
+                expect(res.body).toEqual(
+                    expect.objectContaining({
+                        id: id,
+                        name: 'First training',
+                    })
+                );
             });
     });
 });
