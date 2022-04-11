9245bff2ff37ab2ad88409e85177db696427196d
Adrian Karwowski <adziok1998@gmail.com>
2022-04-11 21:38:25 +0200

    feat: remove InMemoryDbs

--- packages/backend/test/common/createTesingApp.ts
+++ packages/backend/test/common/createTesingApp.ts
@@ -4,10 +4,6 @@ import { UserSessionFactory } from './UserSessionFactory';
 import { AppModule } from '../../src/app.module';
 import { JwtVerifier } from '../../src/modules/auth/JwtVerifier';
 import { mockedJwtVerifier } from './mocks';
-import { TrainingRepository } from '../../src/modules/training/application/repositories/TrainingRepository';
-import { ExerciseRepository } from '../../src/modules/training/application/repositories/ExerciseRepository';
-import { InMemoryTrainingRepository } from '../../src/modules/training/infra/database/InMemoryTrainingRepository';
-import { InMemoryExerciseRepository } from '../../src/modules/training/infra/database/InMemoryExerciseRepository';
 
 export const createTestingApp = async () => {
     // const mongo = await new GenericContainer('mongo').withExposedPorts(DEFAULT_MONGO_PORT).start();
@@ -16,11 +12,7 @@ export const createTestingApp = async () => {
         imports: [AppModule],
     })
         .overrideProvider(JwtVerifier)
-        .useValue(mockedJwtVerifier)
-        .overrideProvider(TrainingRepository)
-        .useValue(new InMemoryTrainingRepository({}))
-        .overrideProvider(ExerciseRepository)
-        .useValue(new InMemoryExerciseRepository({}));
+        .useValue(mockedJwtVerifier);
 
     const app = (await moduleFixture.compile()).createNestApplication();
     attachPipes(app);
