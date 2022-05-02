ff21861cf2275e875b819184601f39734daecdcd
Adrian Karwowski <adziok1998@gmail.com>
2022-05-02 10:04:34 +0200

    feat: expose exercise methods in facade

--- packages/backend/src/modules/training/TrainingFacade.ts
+++ packages/backend/src/modules/training/TrainingFacade.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { IPaginationDto, IPaginationQueryDto, ITrainingDto } from '@trening-tracker/shared';
+import { IExerciseDto, IPaginationDto, IPaginationQueryDto, ITrainingDto } from '@trening-tracker/shared';
 import { ExerciseService } from './application/ExerciseService';
 import { TrainingService } from './application/TrainingService';
-import { ICreateTraining, IUpdateTraining } from './interfaces';
+import { ICreateExerciseInTraining, ICreateTraining, IRemoveExerciseFromTraining, IUpdateTraining } from './interfaces';
 import { TrainingReadService } from './TrainingReadService';
+import { ExerciseReadService } from './ExerciseReadService';
 
 @Injectable()
 export class TrainingFacade {
     constructor(
         private readonly exerciseService: ExerciseService,
         private readonly trainingService: TrainingService,
-        private readonly trainingReadService: TrainingReadService
+        private readonly trainingReadService: TrainingReadService,
+        private readonly exerciseReadService: ExerciseReadService
     ) {}
 
     createTraining(trainingDto: ICreateTraining): Promise<string> {
@@ -21,6 +23,14 @@ export class TrainingFacade {
         return this.trainingService.updateTraining(trainingDto);
     }
 
+    createExerciseInTraining(exercise: ICreateExerciseInTraining): Promise<string> {
+        return this.exerciseService.createExerciseInTraining(exercise);
+    }
+
+    removeExerciseFromTraining(exercise: IRemoveExerciseFromTraining): Promise<string> {
+        return this.exerciseService.removeExerciseFromTraining(exercise);
+    }
+
     getAccountTrainingById(accountId: string, id: string): Promise<ITrainingDto> {
         return this.trainingReadService.getAccountTrainingById(accountId, id);
     }
@@ -28,6 +38,20 @@ export class TrainingFacade {
     async listTrainings(accountId: string, pagination: IPaginationQueryDto): Promise<IPaginationDto<ITrainingDto>> {
         const nodes = await this.trainingReadService.listAccountTrainings(accountId, pagination);
         const totalCount = await this.trainingReadService.count(accountId);
+        return {
+            nodes,
+            totalCount,
+            nextUrl: '',
+            skip: pagination.skip,
+        };
+    }
+
+    async listTrainingExercises(
+        props: { accountId: string; trainingId: string },
+        pagination: IPaginationQueryDto
+    ): Promise<IPaginationDto<IExerciseDto>> {
+        const nodes = await this.exerciseReadService.listTrainingExercises(props, pagination);
+        const totalCount = await this.exerciseReadService.count(props);
         return {
             nodes,
             totalCount,
