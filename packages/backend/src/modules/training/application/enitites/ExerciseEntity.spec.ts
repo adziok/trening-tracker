ff57bb8476cec685b7bb163cbba60ca21055fcd1
Adrian Karwowski <adziok1998@gmail.com>
2022-06-14 19:06:53 +0200

    test(exercise): fix names

--- packages/backend/src/modules/training/application/enitites/ExerciseEntity.spec.ts
+++ packages/backend/src/modules/training/application/enitites/ExerciseEntity.spec.ts
@@ -4,18 +4,18 @@ import { ExerciseSeriesEntity } from './ExerciseSeriesEntity';
 
 describe('ExerciseEntity', () => {
     it('should create an instance of the object using factory method', () => {
-        const instance = ExerciseEntity.create({ name: 'First training', trainingId: UniqueEntityId.generate() });
+        const instance = ExerciseEntity.create({ name: 'First exercise', trainingId: UniqueEntityId.generate() });
         expect(instance.props).toEqual(
             expect.objectContaining({
-                name: 'First training',
+                name: 'First exercise',
                 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                 trainingId: expect.any(UniqueEntityId),
             })
         );
     });
 
-    it('should add a series to the training', () => {
-        const instance = ExerciseEntity.create({ name: 'First training', trainingId: UniqueEntityId.generate() });
+    it('should add a series to the exercise', () => {
+        const instance = ExerciseEntity.create({ name: 'First exercise', trainingId: UniqueEntityId.generate() });
 
         const series = ExerciseSeriesEntity.create({
             reps: 10,
@@ -27,7 +27,7 @@ describe('ExerciseEntity', () => {
     });
 
     it('should ignore duplicates when we add series', () => {
-        const instance = ExerciseEntity.create({ name: 'First training', trainingId: UniqueEntityId.generate() });
+        const instance = ExerciseEntity.create({ name: 'First exercise', trainingId: UniqueEntityId.generate() });
 
         const series = ExerciseSeriesEntity.create({
             reps: 10,
@@ -39,8 +39,8 @@ describe('ExerciseEntity', () => {
         expect(instance.props.series).toHaveLength(1);
     });
 
-    it('should remove a series from the training ', () => {
-        const instance = ExerciseEntity.create({ name: 'First training', trainingId: UniqueEntityId.generate() });
+    it('should remove a series from the exercise ', () => {
+        const instance = ExerciseEntity.create({ name: 'First exercise', trainingId: UniqueEntityId.generate() });
 
         const series = ExerciseSeriesEntity.create({
             reps: 10,
@@ -54,7 +54,7 @@ describe('ExerciseEntity', () => {
     });
 
     it('should throw when trying to remove not existing series', () => {
-        const instance = ExerciseEntity.create({ name: 'First training', trainingId: UniqueEntityId.generate() });
+        const instance = ExerciseEntity.create({ name: 'First exercise', trainingId: UniqueEntityId.generate() });
 
         const series = ExerciseSeriesEntity.create({
             reps: 10,
