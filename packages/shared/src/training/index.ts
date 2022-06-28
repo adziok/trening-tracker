5ffb0e034685d605148cded219605b0eeca5cb04
Adrian Karwowski <adziok1998@gmail.com>
2022-06-28 13:40:12 +0200

    feat: update add series to exercise type

--- packages/shared/src/training/index.ts
+++ packages/shared/src/training/index.ts
@@ -39,6 +39,8 @@ export type ICreateExerciseInTrainingDto = {
 export type IAddSeriesToExerciseDto = {
     reps: number;
     weight: number;
+    trainingId: string;
+    exerciseId: string;
 };
 
 export type IRemoveExerciseFromTrainingDto = {
