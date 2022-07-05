a612c551bdc07e486513e520fefd3bbb974d9191
Adrian Karwowski <adziok1998@gmail.com>
2022-07-05 17:33:17 +0200

    test: prepare a new UserSessionFactory

--- packages/backend/test/common/UserSessionFactory.ts
+++ packages/backend/test/common/UserSessionFactory.ts
@@ -1,9 +1,10 @@
 import { INestApplication } from '@nestjs/common';
 import { randomUUID } from 'crypto';
-import { agent, SuperAgentTest } from 'supertest';
+import { agent } from 'supertest';
 import { AccountFacade } from '../../src/modules/accounts/application/AccountFacade';
+import * as api from '../temp/sdk';
 
-export type UserSession = SuperAgentTest & { authorizationHeaders: () => Record<string, string> };
+export type UserSession = typeof api;
 
 export class UserSessionFactory {
     constructor(private app: INestApplication) {}
@@ -19,10 +20,43 @@ export class UserSessionFactory {
         if (account.hasError()) {
             throw new Error('Can not create UserSession');
         }
-        const httpAgent = agent(this.app.getHttpServer());
 
-        return Object.assign(httpAgent, {
-            authorizationHeaders: () => ({ Authorization: `Bearer ${providerId}` }),
-        });
+        api.defaults.headers = {
+            Authorization: `Bearer ${providerId}`,
+        };
+        api.defaults.fetch = supertestFetch(this.app);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        return api;
     }
 }
+
+const supertestFetch = (app: INestApplication) => {
+    const request = agent(app.getHttpServer());
+    return (
+        url: string,
+        data: {
+            method: string;
+            headers: Record<string, string>;
+            body: string;
+        }
+    ) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return request[data.method?.toLowerCase() || 'get'](url)
+            .set(data.headers)
+            .send(data.body && JSON.parse(data.body))
+            .then((response): Partial<Response> => {
+                return {
+                    text: () => Promise.resolve(response.text),
+                    json: () => Promise.resolve(response.body),
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                    headers: new Headers(response.headers),
+                    ok: !response?.body?.errors,
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    body: response.body,
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    status: response.status,
+                };
+            });
+    };
+};
