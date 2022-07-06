7e300cebb86830fdce8d9efb6627757484f99ce0
Adrian Karwowski <adziok1998@gmail.com>
2022-07-06 09:02:42 +0200

    feat: add validation pipe to open api spec generator

--- packages/backend/src/generate-open-api.ts
+++ packages/backend/src/generate-open-api.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as fs from 'fs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.useGlobalPipes(new ValidationPipe());
 
-    const config = new DocumentBuilder().setTitle('Training tracker api').build();
+    const config = new DocumentBuilder().setTitle('Training tracker api').setVersion('1.0.0').build();
     const document = SwaggerModule.createDocument(app, config);
     fs.writeFileSync('./open-api.json', JSON.stringify(document));
 }
